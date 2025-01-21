5079ca9e110d1b4f014f39f96073da1dd849ceae
Iraitz Aurrekoetxea <iraitz.aurrekoetxea.jimenez@zabalburu.org>
2025-01-21 13:52:45 +0100

    Inicio Actividad

---
+++ Actividad16_II/src/main/java/com/zabalburu/daw1/actividad16_ii/modelo/Evento.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.zabalburu.daw1.actividad16_ii.modelo;
+
+/**
+ *
+ * @author DAW1
+ */
+public class Evento {
+    
+}
